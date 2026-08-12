e86d3375159d16b5e264e147a5f4f03af849f5fb
Ömer Eroğlu <omereroglu1923@gmail.com>
2026-08-12 13:35:58 +0300

    Refactor: extract repeated user creation into beforeEach hook

--- tests/Feature/ProductsTest.php
+++ tests/Feature/ProductsTest.php
@@ -6,23 +6,24 @@ use Illuminate\Pagination\LengthAwarePaginator;
 use function Pest\Laravel\actingAs;
 use function Pest\Laravel\get;
 
-test('homepage contains empty table', function () {
-    $user = User::factory()->create();
+beforeEach(function () {
+    $this->user = User::factory()->create();
+});
 
-    actingAs($user)
+test('homepage contains empty table', function () {
+    actingAs($this->user)
         ->get('/products')
         ->assertStatus(200)
         ->assertSee(__('No products found'));
 });
 
 test('homepage contains non empty table', function () {
-    $user = User::factory()->create();
     $product = Product::create([
         'name'  => 'Product 1',
         'price' => 123,
     ]);
 
-    actingAs($user)
+    actingAs($this->user)
         ->get('/products')
         ->assertStatus(200)
         ->assertDontSee(__('No products found'))
@@ -33,11 +34,10 @@ test('homepage contains non empty table', function () {
 });
 
 test('paginated products table doesnt contain 11th record', function () {
-    $user = User::factory()->create();
     $products = Product::factory(11)->create();
     $lastProduct = $products->last();
 
-    actingAs($user)
+    actingAs($this->user)
         ->get('/products')
         ->assertStatus(200)
         ->assertViewHas('products', function (LengthAwarePaginator $collection) use ($lastProduct) {
