a690c42af2755b04de65aed7a634498551391619
Ömer Eroğlu <omereroglu1923@gmail.com>
2026-08-12 11:51:30 +0300

    Add assertViewHas for reliable product data testing (avoid assertSee false positive)

--- tests/Feature/ProductsTest.php
+++ tests/Feature/ProductsTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Product;
+use Illuminate\Database\Eloquent\Collection;
 use function Pest\Laravel\get;
 
 test('homepage contains empty table', function () {
@@ -10,12 +11,16 @@ test('homepage contains empty table', function () {
 });
 
 test('homepage contains non empty table', function () {
-    Product::create([
+    $product = Product::create([
         'name'  => 'Product 1',
         'price' => 123,
     ]);
 
     get('/products')
         ->assertStatus(200)
-        ->assertDontSee(__('No products found'));
+        ->assertDontSee(__('No products found'))
+        ->assertSee('Product 1')
+        ->assertViewHas('products', function (Collection $collection) use ($product) {
+            return $collection->contains($product);
+        });
 });
