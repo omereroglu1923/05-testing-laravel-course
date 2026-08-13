c1b12a3fe38424c39e5110aa27a3a40098e748fa
Ömer Eroğlu <omereroglu1923@gmail.com>
2026-08-13 15:04:42 +0300

    Rename homepage test to clarify it covers a critical feature (uptime check)

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,6 @@
 <?php
 
-it('returns a successful response', function () {
+test('homepage returns a successful response', function () {
     $response = $this->get('/');
-
     $response->assertStatus(200);
 });
